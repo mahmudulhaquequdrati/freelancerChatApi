373b3a617fc0b5b65544a4c6e1827de7e0d49e58
mahmudulhaquequdrati <freelancermahmudqudrati@gmail.com>
2022-05-05 13:16:46 +0600

    user post success

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-        type: String
-    },
-    name:{
-        type:String
-    }
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
   },
-  { timestamps: true }
-);
+  name: {
+    type: String,
+  },
+  img: {
+    type: String,
+  },
+});
 
 module.exports = mongoose.model("User", UserSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,30 @@
 const User = require("../models/User");
 const router = require("express").Router();
 
-router.get('/:userEmail', async(req,res)=>{
-    try {
-        const userEmail = req.params.userEmail;
-        const user = await User.find({email: userEmail})
-        res.status(200).json(user)
-    } catch (error) {
-         res.status(500).json(error);
-    }
-})
-router.get('/', async(req,res)=>{
-    try {
-        
-        const user = await User.find()
-        res.status(200).json(user)
-    } catch (error) {
-         res.status(500).json(error);
-    }
-})
+router.get("/:userEmail", async (req, res) => {
+  try {
+    const userEmail = req.params.userEmail;
+    const user = await User.find({ email: userEmail });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+router.get("/", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+router.post("/", async (req, res) => {
+  const newUser = new User(req.body);
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 module.exports = router;
